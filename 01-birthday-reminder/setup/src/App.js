f7c6ab3f16b48cd373a3cf251ed6898b5c738ea0
vaisakh <vaisakh.narayanan149@gmail.com>
2022-08-15 23:29:10 +0530

    birtday remainder

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,23 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPepole] = useState(data);
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} birtdays today</h3>
+        <List people={people} />
+        <button
+          onClick={() => {
+            setPepole([])
+          }}
+        >
+          Clear all
+        </button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
